10d6d8561cfba1901a35f0f16162b2550fee0bbb
Alex C Moody <alexmoody@unm.edu>
2017-05-23 11:34:27 -0600

    Some plots
    Needs a lot of fixing. Plots were made for SHF QC and Marcy's ET
    conference

---
+++ plots/plot_qc_shf.m
@@ -0,0 +1,203 @@
+function plot_qc_shf(  sitecode, ...
+    year, ...
+    timestamp, ...
+    Tair_hmp, ...
+    RH_hmp, ...
+    VPD, ...
+    Tair_sonic, ...
+    H2O_irga, ...
+    precip,...
+    NR_tot )
+% plot_qc_radiation - makes diagnostic figures showing met variables
+%
+% Called from UNM_RBD.m
+%
+% FIXME - this needs to be finished....
+%
+% INPUTS
+%    sitecode: UNM_sites object; specifies the site to show
+%    year: four-digit year: specifies the year to show
+%    dataOrig: NxM numeric: the original, unshifted fluxall data
+%    dataShifted: NxM numeric: the shifted fluxall data.
+%
+% OUTPUTS
+%    2 figure handles
+%
+% SEE ALSO
+%    UNM_sites, dataset, UNM_fix_datalogger_timestamps, shift_data
+%
+% author: Gregory E. Maurer, UNM, February 2015
+
+[ this_year, ~, ~ ] = datevec( now );
+
+% -----
+% define optional inputs, with defaults and typechecking
+% -----
+% args = inputParser;
+% args.addRequired( 'sitecode', @(x) ( isintval( x ) | isa( x, 'UNM_sites' ) ) );
+% args.addRequired( 'year', ...
+%     @(x) ( isintval( x ) & ( x >= 2006 ) & ( x <= this_year ) ...
+%     ) );
+% args.addRequired( 'dataTable', @istable );
+%args.addRequired( 'dataShifted',  @istable );
+
+% parse optional inputs
+% args.parse( sitecode, year, dataTable );
+% 
+% sitecode = args.Results.sitecode;
+% year = args.Results.year;
+% dataT = args.Results.dataTable;
+
+%warning( 'Met diagnostic not done yet!' );
+
+%return
+
+allTimestamps = timestamp;
+
+% Number of days to plot radiation for
+metDays = 10; % July 10
+
+% Day of month to center data around
+dom = 15;
+
+% Colors
+rhhmp1_c = [ 0 168/255 119/255 ];
+rhhmp2_c = [ 0 106/255 78/255 ];
+tahmp1_c = [ 255/255 140/255 0 ];
+tahmp2_c = [ 255/255 88/255 0 ];
+irgah2o_c = [ 0 147/255 175/255 ];
+precip_c = [ 0 24/255 168/255 ];
+tason_c = [237/255 28/255 36/255 ];
+%rnet_c = [ 0 0 0 ]
+
+hmp2 = false;
+precip2 = false;
+
+% ============================ FIGURE 1 =================================
+
+% Quick figure to check whether PPFD and SWin are scaled well.
+% PPFD (PAR) converts to SWin using a .48 scaling factor, and it should
+% normally be slightly larger than our sw_incoming measurement, but below
+% the top of the atmosphere max SWin ( around 1300 W/m2 ).
+
+h_fig1 = figure( 'Name', 'Met: 1 year T and RH', ...
+    'Position', [ 100 100 800 750 ] );
+h_ax( 1 ) = subplot( 6, 1, 1:2);
+plot(timestamp, Tair_hmp, '-', 'Color', tahmp1_c );
+hold on;
+plot( timestamp, Tair_sonic - 273.15, '-', 'Color', tason_c );
+if hmp2
+    plot( timestamp, Tair_hmp2, ':', 'Color', tahmp2_c );
+    legend( 'Tair (hmp 1)', 'Tair (sonic anem.)', ...
+        'Tair (hmp 2)', 'Location', 'SouthWest' );
+else
+    legend( 'Tair (hmp 1)', 'Tair (sonic anem.)', ...
+        'Location', 'NorthWest' );
+end
+datetick('x','mmm dd', 'keepticks');
+ylabel( 'Tair ( degrees C )' );
+ylim( [-30, 40] );
+xlim([ min( timestamp ) max( timestamp ) ]);
+Tdiff = (Tair_sonic - 273.15) - Tair_hmp ;
+title(['Mean difference = ', num2str( nanmean( Tdiff ) ),...
+    ' Mode = ', num2str( mode( Tdiff ) )]  );
+
+h_ax( 2 ) = subplot( 6, 1, 3:4 );
+[yy_ax, h_line1, h_line2 ] = plotyy( timestamp, RH_hmp, timestamp, VPD );
+set( h_line1, 'Color', rhhmp1_c );
+set( h_line2, 'Color', 'Black', 'LineStyle', ':');
+if hmp2
+    plot( yy_ax( 1 ), timestamp, RH_hmp2, ':', 'Color', rhhmp2_c );
+    legend( 'RH (hmp 1)', 'VPD', 'RH (hmp 2)', 'Location', 'NorthWest' );
+else
+    legend( 'RH', 'VPD', 'Location', 'NorthWest' );
+end
+%ylabel( 'RH ( % )' );
+ylim( yy_ax( 1 ), [-2, 102] );
+ylim( yy_ax( 2 ), [0, 100] );
+xlim( yy_ax( 1 ), [ min( timestamp ) max( timestamp ) ]);
+xlim( yy_ax( 2 ), [ min( timestamp ) max( timestamp ) ]);
+datetick('x','mmm dd', 'keepticks');
+
+h_ax( 3 ) = subplot( 6, 1, 5 );
+plot( timestamp, precip, '-', 'Color', [ 0.2 0.2 0.2 ] );
+if precip2
+    plot( timestamp, precip2, '-', 'Color', [ 0.5 0.5 0.5 ] );
+    legend( 'RH (hmp 1)', 'RH (hmp 2)', 'Location', 'SouthWest' );
+end
+ylabel( {'Precip', '( mm )'} );
+%ylim( [-2, 102] );
+xlim([ min( timestamp ) max( timestamp ) ]);
+datetick('x','mmm dd', 'keepticks');
+
+% Load the PRISM precip for this year if it is available
+try
+    siteID = get_site_name( sitecode, 'aflx' );
+    PRISM_precip = UNM_parse_PRISM_met_data( siteID, year );
+catch
+    PRISM_precip = table( timestamp, precip * NaN, ...
+        'VariableNames', { 'timestamp', 'Precip' } );
+end
+
+h_ax( 4 ) = subplot( 6, 1, 6 );
+plot( PRISM_precip.timestamp, PRISM_precip.Precip, '-', 'Color', precip_c );
+xlabel( 'Timestamp');
+ylabel( {'PRISM', '( mm )'} );
+%ylim( [-5, 102] );
+xlim([ min( timestamp ) max( timestamp ) ]);
+datetick('x','mmm dd', 'keepticks', 'keeplimits' );
+
+linkaxes( h_ax, 'x' );
+
+% ============================ FIGURE 2 =================================
+% Tair in each month of year
+
+% Set up figure window
+h_fig2 = figure( 'Name', ...
+    sprintf('%s %d Met diagnostics 2/2', ...
+    get_site_name( sitecode ), year ), ...
+    'Position', [100 100 1250 950], 'Visible', 'on' );
+
+% Arrays of data and labels
+xLabelList = { 'January', 'February', 'March', 'April', 'May', 'June', ...
+    'July', 'August', 'September', 'October', 'November', 'December' };
+
+% Dont plot months without the days we need
+[ ~, monthsPresent, daysPresent, ~, ~, ~ ] = datevec( allTimestamps );
+fullMonthsPresent = monthsPresent( daysPresent >= dom + 5 ); 
+
+for i = 1:max( fullMonthsPresent )
+    % Get day numbers, mean data for 2 variables, and solar events for 
+    % the first week of month (i)
+    repday = datenum( year, i, dom );
+    
+    startDay = repday - metDays/2;
+    endDay = repday + metDays/2;
+    metTest = allTimestamps >= startDay & allTimestamps <= endDay;
+    
+    times = allTimestamps( metTest ) - floor( allTimestamps( metTest ));
+    times = times * 24;
+    % Create subplot and draw dual y axis with original (unshifted) data
+    hAx = subplot( 3, 4, i );
+    hLine1 = plot( times, Tair_hmp( metTest ), '.', ...
+        'Color', tahmp1_c);
+    hold on;
+    hLine2 = plot( times, Tair_sonic( metTest ) - 273.15, '.', ...
+        'Color', tason_c );
+    xlabel( xLabelList{ i } );
+    ylim([ -30 40 ]);
+    xlim([ 0 24 ]);
+    hold on;
+
+    if i == 1
+        lh = legend( [ hLine1, hLine2 ], ...
+            'Tair (hmp1)', 'Tair (sonic)', ...
+            'Location', 'NorthWest', 'Orientation', 'horizontal' );
+        set( lh , 'Position', get( lh, 'Position') + [0 .05 0 0 ] );
+    end
+
+    % Label axes
+    if i == 1 || i == 5 || i == 9 ;
+        ylabel( hAx( 1 ), 'Tair ( degrees )');
+    end
+end
---
+++ scripts/plot_ET_bar_allyears.m
@@ -0,0 +1,178 @@
+% LE_2_ET - Convert 30min latent heat flux to ET ( W/m^2 to mm/s ) and sum (integrate)
+%     for each 30min period. Note that this method uses the full day of ET,
+%     rather than just daytime values
+
+setenv('AFLX','C:\Code\NMEG_utils\processed_data\daily_aflx\FLUXNET2015_c');
+% Ameriflux sitelist 
+sitelist={'Seg', 'Ses', 'Wjs', 'Mpj' , 'Vcp' ,'Vcm'};
+yearlist=( 2007:2016 );
+convert2mm = false; % I believe the Keenan uncertainty files are in mm already.
+if exist('all_data')
+load_data = false;
+else
+    load_data = true;
+end
+
+if load_data 
+% ------------- LOAD FILES------------------
+all_data = struct('site',sitelist,'data',[]);
+for i = 1:length(sitelist)    
+    site = all_data(i).site;
+    % Now ameriflux files
+    fname= fullfile( getenv('AFLX'),strcat('US-',site,'_daily_aflx.csv') );
+    data = parse_aflx_daily_file(fname);
+    % Add Water year timestamp
+    [data.wyear , data.wyDOY] = ts2wateryear(data.TIMESTAMP);
+    % Put this in a structure of all data
+    all_data(i).data = data;
+end
+end
+
+% Get yearly site ET and PPT
+yearlist = 2006:2016;
+meanET_t = array2table(yearlist','VariableNames',{'wyear'});
+meanP_t =  array2table(yearlist','VariableNames',{'wyear'});
+meanETSE_t = array2table(yearlist','VariableNames',{'wyear'});
+meanPSE_t = array2table(yearlist','VariableNames',{'wyear'});
+for i = 1:length(sitelist)
+    
+    site = all_data(i).site;
+    data = all_data(i).data;
+    
+    % Calculate yearly means
+    [uniqueYears,idxToUnique,idxFromUniqueBackToAll] = unique(data.wyear);
+    yearlyMeanET = horzcat(uniqueYears ,accumarray(idxFromUniqueBackToAll,data.ET_mm_dayint,[],@nanmean));
+    yearlyMeanET = array2table(yearlyMeanET,'VariableNames',{'wyear',site});
+    meanET_t = outerjoin(meanET_t,yearlyMeanET,'MergeKeys',true);
+    
+    yearlyMeanP =  horzcat(uniqueYears ,accumarray(idxFromUniqueBackToAll,data.P_F_sum,[],@nanmean) );
+    yearlyMeanP =  array2table(yearlyMeanP,'VariableNames',{'wyear',site});
+    meanP_t = outerjoin(meanP_t,yearlyMeanP,'MergeKeys',true)   ;
+    
+    % Calculate yearly mean SE
+    yearlyMeanETSE = horzcat(uniqueYears ,accumarray(idxFromUniqueBackToAll,data.ET_mm_dayint,[],@nanstd));
+    yearlyMeanETSE = array2table(yearlyMeanETSE,'VariableNames',{'wyear',site});
+    meanETSE_t = outerjoin(meanETSE_t,yearlyMeanETSE,'MergeKeys',true);
+    
+    yearlyMeanPSE =  horzcat(uniqueYears ,accumarray(idxFromUniqueBackToAll,data.P_F_sum,[],@nanstd) );
+    yearlyMeanPSE =  array2table(yearlyMeanPSE,'VariableNames',{'wyear',site});
+    meanPSE_t = outerjoin(meanPSE_t,yearlyMeanPSE,'MergeKeys',true)   ;
+end
+
+ETPratio = [  meanET_t{:,2:end}./meanP_t{:,2:end}];
+ETPratio = array2table(ETPratio,'VariableNames',sitelist) 
+
+% We only have a partial (272/365 days)  water year for 2006. Scale by
+% portion of the year?
+
+
+c = categorical(meanP_t.Properties.VariableNames(2:end));
+for i = 1: length(yearlist)
+   
+    year = meanP_t{i,1};
+   
+    P = meanP_t{ i , 2: end }';
+    Perr = meanPSE_t{ i , 2: end }';
+    ET = meanET_t{ i , 2: end }';
+    ETerr = meanETSE_t{ i , 2: end }';
+    
+    y = [ET, P ]; 
+    errY = [ETerr,Perr];          
+    
+    %figure; hold on
+    %[bar1 barerr] = barwitherr(errY, y);% Plot with errorbars 
+    bar1 = bar( y );
+    set(gca,'XTickLabel',sitelist)
+    set(gca,'YLim',[0 2.5])
+    set(bar1(1),'FaceColor',[224/255 194/255 85/255],'DisplayName','ET')
+    set(bar1(2),'FaceColor',[23/255 162/255 209/255],'DisplayName','P');
+   
+    title(sprintf('WY %d', year))
+    ylabel('Mean Annual ET or P (mm d^{-1})')
+    legend('ET','P','Location','northwest') 
+    
+    destfile = fullfile(getenv('FLUXROOT'),'Plots',...
+        'NMWRRI_ET',sprintf('MeanAnnualETP_%d',year));
+    saveas( gcf , destfile, 'png')
+    %hold off
+end
+
+% ----------- Mean annual ET/P ratio
+ETPratio = [  meanET_t{:,2:end}./meanP_t{:,2:end}];
+
+gcolour=[0.9,0.5,0.0];
+ngcolour= [0.9, 0.8, 0.0];
+scolour=[0.6, 0.2, 0];
+jcolour=[0.25, 1.0, 0.0];
+pjcolour=[0.0, 0.5, 0.0];
+pjgcolour=[0.0, 0.85, 0.0];
+pcolour=[0.5, 0.5, 1.0];
+mcolour=[0.0, 0.0, 0.6];
+nmcolour=[0.3, 0.0, 0.5];
+
+figure2 = figure('Position',[1 550 1906 525]);
+bar2 = bar(ETPratio);
+set(gca,'XTickLabel',yearlist,'YGrid','on');
+%set(gca,'YLim',[0 2.5])
+set(bar2(1),'FaceColor',gcolour,'DisplayName','Seg')
+set(bar2(2),'FaceColor',scolour,'DisplayName','Ses');
+set(bar2(3),'FaceColor',jcolour,'DisplayName','Wjs')
+set(bar2(4),'FaceColor',pjcolour,'DisplayName','Mpj')
+set(bar2(5),'FaceColor',pcolour,'DisplayName','Vcp')
+set(bar2(6),'FaceColor',mcolour,'DisplayName','Vcm')
+
+%title(sprintf('WY %d', year))
+ylabel(' ET/P ')
+legend(gca,'show','Location','best');
+
+destfile = fullfile(getenv('FLUXROOT'),'Plots',...
+        'NMWRRI_ET',sprintf('ETP_ratio',year));
+    saveas( gcf , destfile, 'png')
+
+
+% 
+% 
+% meanLE = array2table( all_accum_le , ...
+%     'VariableNames',{'WaterYear' 'LEf_Mpj', 'LEun_Mpj','LEf_Mpg','LEun_Mpg',...
+%     'LEf_daytime_Mpj', 'LEunsd_Mpj','LEf_daytime_Mpg','LEunsd_Mpg' } );
+% 
+% fname = fullfile(getenv('FLUXROOT'),'LE_mean_allsites.txt');
+% writetable( meanLE, fname, 'delimiter',',');
+%         
+%[uniqueYears,idxToUnique,idxFromUniqueBackToAll] = unique(Mpg.Year);
+% cumulativeLE = accumarray(idxFromUniqueBackToAll,Mpg.LEf,[],@nanmean);
+%cumulativeLE = accumarray(idxFromUniqueBackToAll,Mpg.LEf,[],@cumsum);
+
+
+
+
+% if convert2mm
+% % -------------
+% % GIRDLE CALCS
+% % ------------
+% % Calculate latent heat of vaporization 
+% Mpg.Lv = ( 2.501 - 0.00237 * ( Mpg.TA_f ) ) .* 10^3;
+% et_mms = ( 1 \ ( Mpg.Lv .* 1000 )) .* Mpg.LEf;
+% % Integrate over full day
+% Mpg.ET_mm_int = et_mms;
+% 
+% % Calculate for uncertainties
+% etu_mms = ( 1 \( Mpg.Lv * 1000 )) .* Mpg.LEu;
+% % Integrate over full day
+% Mpg.ETu_mm_int = etu_mms;
+% 
+% 
+% % -------------
+% % CONTROL CALCS
+% % ------------
+% % Calculate latent heat of vaporization 
+% Mpj.Lv = ( 2.501 - 0.00237 * ( Mpj.TA_f ) ) .* 10^3;
+% et_mms = ( 1 / ( Mpj.Lv * 1000 )) .* Mpg.LEf;
+% % Integrate over full day
+% Mpj.ET_mm_int = et_mms.*86400;
+% 
+% % Calculate for uncertainties
+% etu_mms = ( 1 / ( Mpj.Lv * 1000 )) .* Mpj.LEu;
+% % Integrate over full day
+% Mpj.ETu_mm_int = etu_mms;
+% end
