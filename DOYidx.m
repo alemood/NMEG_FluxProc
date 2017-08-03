cb6596697e6f26dca174a008f5940207e343c511
Alex Moody <alexmoody@unm.edu>
2017-08-03 10:53:05 -0600

    There is technically no DOY == 1
    Since timestamps refer to the prior 30 minutes, the first decimal day of
    year will always be 1.0208, corresponding to January 1st @ 0030.

--- DOYidx.m
+++ DOYidx.m
@@ -7,6 +7,8 @@ function idx = DOYidx( DOY )
 %
 % Useful for finding a particular time range within an annual 30-minute dataset.
 %
+%
+%
 % USAGE
 %    idx = DOYidx( DOY );
 %
@@ -17,10 +19,15 @@ function idx = DOYidx( DOY )
 %    idx: index of DOY in an annual thirty-minute dataset.  0 <= idx <= 17569.
 %
 % author: Timothy W. Hilton, UNM, June 2012
-
-if ( DOY < 0 ) | ( DOY >= 367 )
-    error( 'DOY must satisfy 0 <= DOY < 367' );
+warning('This may break index requests if DOY = 1. Fix to 1.0208 as you encounter them')
+if ( DOY < 1.0208 ) | ( DOY >= 367 )
+    error( 'DOY must satisfy 1.0208 <= DOY < 367' );
 end
 
+
+
 obs_per_day = 48;
-idx = int32( ( obs_per_day * DOY ) - obs_per_day + 1 );
+%idx = int32( ( obs_per_day * DOY ) - obs_per_day + 1 ); 
+% The above line was offsetting indices by a half hour forward. Measurement
+% appeared to happen later than it should.
+idx = int32( ( obs_per_day * DOY ) - obs_per_day);
